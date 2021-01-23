a101580247ced61852d517cc20a2d80ca66d2d2b
Michael Hayden <mhayden@michaelhayden.net>
2021-01-23 15:45:18 -0700

    update for test env

--- config/test.ts
+++ config/test.ts
@@ -2,9 +2,9 @@ export default {
   database: {
     type: process.env.DB_TYPE || 'postgres',
     host: process.env.DB_HOST_TEST || 'localhost',
-    port: process.env.DB_PORT_TEST || 6434,
-    username: process.env.DB_USERNAME_TEST || 'cleo',
-    password: process.env.DB_PASSWORD_TEST || 'cleo',
+    port: process.env.DB_PORT_TEST || 5432,
+    username: process.env.DB_USERNAME_TEST || 'dev',
+    password: process.env.DB_PASSWORD_TEST || 'password123',
     database: process.env.DB_DATABASE_TEST || 'csv_processor_test',
     logging: process.env.DB_LOGGING_TEST || false,
     migrationsRun: true,
