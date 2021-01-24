0b0c3c84a20a72d896e6d1d72a5f4773b93ea637
Michael Hayden <mhayden@michaelhayden.net>
2021-01-24 09:31:15 -0700

    pipe commands in circleci config

--- config/test.ts
+++ config/test.ts
@@ -2,7 +2,7 @@ export default {
   database: {
     type: process.env.DB_TYPE || 'postgres',
     host: process.env.DB_HOST_TEST || 'localhost',
-    port: process.env.DB_PORT_TEST || 6434,
+    port: process.env.DB_PORT_TEST || 5432,
     username: process.env.DB_USERNAME_TEST || 'dev',
     password: process.env.DB_PASSWORD_TEST || 'password123',
     database: process.env.DB_DATABASE_TEST || 'csv_processor_test',
